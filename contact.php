d13dc985a5b48dd2c0eb1f2a5069552278309180
lTejas Ulawekar <tejasau21hcompe@student.mes.ac.in>
2024-07-29 22:02:50 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,142 @@
+<?php
+// Initialize the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php'?>
+		<!-- Hero-area -->
+		<div class="hero-area section">
+
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/bgc2.jpg); " ></div>
+			<!-- /Backgound Image -->
+
+			<div class="container">
+				<div class="row" >
+					<div class="col-md-10 col-md-offset-1 text-center">
+						<ul class="hero-area-tree">
+							<li><a href="main.html">Home</a></li>
+							<li>Contact</li>
+						</ul>
+						<h1 class="white-text">Get In Touch</h1>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- /Hero-area -->
+
+		<!-- Contact -->
+		<div id="contact" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- contact form -->
+					<div class="col-md-6">
+						<div class="contact-form">
+							<h4>Send A Message</h4>
+							<form>
+								<input class="input" type="text" name="name" placeholder="Name">
+								<input class="input" type="email" name="email" placeholder="Email">
+								<input class="input" type="text" name="subject" placeholder="Subject">
+								<textarea class="input" name="message" placeholder="Enter your Message"></textarea>
+								<button class="main-button icon-button pull-right">Send Message</button>
+							</form>
+						</div>
+					</div>
+					<!-- /contact form -->
+
+					<!-- contact information -->
+					<div class="col-md-5 col-md-offset-1">
+						<h4>Contact Information</h4>
+						<ul class="contact-details">
+							<li><i class="fa fa-envelope"></i>Careerrsmith@gmail.com</li>
+							<li><i class="fa fa-phone"></i>8282520007</li>
+							<li><i class="fa fa-map-marker"></i>Panvel</li>
+						</ul>
+
+						<!-- contact map -->
+						<div id="contact-map"></div>
+						<!-- /contact map -->
+
+					</div>
+					<!-- contact information -->
+
+				</div>
+				<!-- /row -->
+
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Contact -->
+
+		
+		<!-- Footer -->
+		<footer id="footer" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- footer logo -->
+					<div class="col-md-6">
+						<div class="footer-logo">
+							<a class="logo" style="font-size: 30px;" href="main.html">Career.smith</a>
+						</div>
+					</div>
+					<!-- footer logo -->
+
+
+				</div>
+				<!-- /row -->
+
+				<!-- row -->
+				<div id="bottom-footer" class="row">
+
+					<!-- social -->
+					<div class="col-md-4 col-md-push-8">
+						
+					</div>
+					<!-- /social -->
+
+					<!-- copyright -->
+					<div class="col-md-8 col-md-pull-4">
+						<div class="footer-copyright">
+							<span>&copy; Copyright 2024. All Rights Reserved. </span>
+						</div>
+					</div>
+					<!-- /copyright -->
+
+				</div>
+				<!-- row -->
+
+			</div>
+			<!-- /container -->
+
+		</footer>
+		<!-- /Footer -->
+		
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+		<!-- /preloader -->
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script src="https://www.google.co.in/maps/place/Panvel,+Navi+Mumbai,+Maharashtra/@18.9889383,73.0319551,12z/data=!3m1!4b1!4m6!3m5!1s0x3be7e83e1f23f23d:0xe3a106c431e3fd0a!8m2!3d18.9894007!4d73.1175162!16zL20vMDV2eF94?entry=ttuexp&sensor=false"></script>
+		<script type="text/javascript" src="js/google-map.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</body>
+</html>
